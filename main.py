69267adf472ef8d11761e812b147c8723f82909d
luismiguel13 <66281445+luismiguel13@users.noreply.github.com>
2022-09-12 08:13:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_PORT'] = 3306
 app.config['MYSQL_DATABASE_USER'] = 'root' #depende del usuario que asignaron en heidiSQL
 app.config['MYSQL_DATABASE_PASSWORD'] = '1234' #depende de la contraseña que asignaron en heidiSQL
-app.config['MYSQL_DATABASE_DB'] = 'petclub'
+app.config['MYSQL_DATABASE_DB'] = 'vetclub'
 
 
 mysql = MySQL()
